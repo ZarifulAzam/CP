84e9424b75e16fc3b904f59a444e3a671d2ba2c9
ZarifulAzam <117255713+ZarifulAzam@users.noreply.github.com>
2023-11-02 22:55:47 +0600

    Add files via upload

---
+++ word.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    string s;
+    int i;
+    cin>>s;
+    int count_lower = 0 , count_upper = 0;
+    for (i = 0; i < s.size(); i++)
+    {
+        if (isupper(s[i]))
+        {
+            count_upper++;
+        }
+        if (islower(s[i]))
+        {
+            count_lower++;
+        }
+    }
+    if (count_upper>count_lower)
+    {
+       for (int i = 0; i < s.size(); i++)
+        {
+            s[i]= tolower(s[i]);
+        }
+
+    }
+    else
+    {
+        for (int i = 0; i < s.size(); i++)
+        {
+            s[i]= tolower(s[i]);
+        }
+
+    }
+    cout<<s<<endl;
+    
+    return 0;
+ }
